c9192527e723f5a864ac74a687d01ee4b1364afe
Hanna Pęciak <103579210+haszpe@users.noreply.github.com>
2024-06-18 21:02:03 +0200

    Data clean v2

--- ER_analysis.R
+++ ER_analysis.R
@@ -41,6 +41,11 @@ colnames(fb_miyo)[4] ="typ posta"
 fb_htc['suma reakcji'] <- rowSums(fb_htc[, c('comments', 'likes', 'shares')], na.rm=T)
 colnames(fb_htc)[4] ="typ posta"
 
+# Outliers handling:
+fb_tolpa <- fb_tolpa[-58, ]
+insta_tolpa[61, 2] <- c('Video')
+
+
 insta_blab['suma reakcji'] <- rowSums(insta_blab[, c('likesCount', 'commentsCount')], na.rm=T)
 insta_kaya['suma reakcji'] <- rowSums(insta_kaya[, c('likesCount', 'commentsCount')], na.rm=T)
 insta_tolpa['suma reakcji'] <- rowSums(insta_tolpa[, c('likesCount', 'commentsCount')], na.rm=T)
@@ -53,12 +58,58 @@ colnames(insta_miyo)[2] ="typ posta"
 colnames(insta_htc)[2] ="typ posta"
 
 
-#for (tab in c('fb_blab', 'fb_kaya', 'fb_tolpa', 'fb_miyo', 'fb_htc', 'insta_blab', 
-#              'insta_kaya', 'insta_tolpa', 'insta_miyo', 'insta_htc')){
-#  tab <- get(tab)
-#  tab$'ER od posta' <- tab$`ER od posta`*100
-#}
+for (tab in c('fb_blab', 'fb_kaya', 'fb_tolpa', 'fb_miyo', 'fb_htc', 'insta_blab', 
+              'insta_kaya', 'insta_tolpa', 'insta_miyo', 'insta_htc')){
+  tab <- get(tab)
+  tab$'ER od posta' <- tab$`ER od posta`*100
+}
 
+fb_blab$source <- c('Facebook')
+fb_blab$brand <- c('BasicLab')
+fb_kaya$source <- c('Facebook')
+fb_kaya$brand <- c('Kaya')
+fb_tolpa$source <- c('Facebook')
+fb_tolpa$brand <- c('Tolpa')
+fb_miyo$source <- c('Facebook')
+fb_miyo$brand <- c('Miyo')
+fb_htc$source <- c('Facebook')
+fb_htc$brand <- c('HTC')
+
+insta_blab$source <- c('Instagram')
+insta_blab$brand <- c('BasicLab')
+insta_kaya$source <- c('Instagram')
+insta_kaya$brand <- c('Kaya')
+insta_tolpa$source <- c('Instagram')
+insta_tolpa$brand <- c('Tolpa')
+insta_miyo$source <- c('Instagram')
+insta_miyo$brand <- c('Miyo')
+insta_htc$source <- c('Instagram')
+insta_htc$brand <- c('HTC')
+
+# Zamień 'Image' na 'Photo
+insta_blab$'typ posta' <- gsub('Image', 'Photo', insta_blab$'typ posta')
+insta_kaya$'typ posta' <- gsub('Image', 'Photo', insta_kaya$'typ posta')
+insta_tolpa$'typ posta' <- gsub('Image', 'Photo', insta_tolpa$'typ posta')
+insta_miyo$'typ posta' <- gsub('Image', 'Photo', insta_miyo$'typ posta')
+insta_htc$'typ posta' <- gsub('Image', 'Photo', insta_htc$'typ posta')
+insta_blab$'typ posta' <- gsub('Sidecar', 'Photo', insta_blab$'typ posta')
+insta_kaya$'typ posta' <- gsub('Sidecar', 'Photo', insta_kaya$'typ posta')
+insta_tolpa$'typ posta' <- gsub('Sidecar', 'Photo', insta_tolpa$'typ posta')
+insta_miyo$'typ posta' <- gsub('Sidecar', 'Photo', insta_miyo$'typ posta')
+insta_htc$'typ posta' <- gsub('Sidecar', 'Photo', insta_htc$'typ posta')
+
+# ER --------------------------------------------------------------------------
+fb_blab$ER <- (fb_blab$'suma reakcji'/n_fb_blab)*100
+fb_kaya$ER <- (fb_kaya$'suma reakcji'/n_fb_kaya)*100
+fb_tolpa$ER <- (fb_tolpa$'suma reakcji'/n_fb_tolpa)*100
+fb_miyo$ER <- (fb_miyo$'suma reakcji'/n_fb_miyo)*100
+fb_htc$ER <- (fb_htc$'suma reakcji'/n_fb_htc)*100
+
+insta_blab$ER <- (insta_blab$'suma reakcji'/n_insta_blab)*100
+insta_kaya$ER <- (insta_kaya$'suma reakcji'/n_insta_kaya)*100
+insta_tolpa$ER <- (insta_tolpa$'suma reakcji'/n_insta_tolpa)*100
+insta_miyo$ER <- (insta_miyo$'suma reakcji'/n_insta_miyo)*100
+insta_htc$ER <- (insta_htc$'suma reakcji'/n_insta_htc)*100
 
 # FB: Sprawdzenie normalności rozkładu zmiennej 'suma reakcji na post' w grupach ---------------------
 for (category in unique(fb_blab$'typ posta')){
@@ -73,9 +124,6 @@ for (category in unique(fb_kaya$'typ posta')){
   cat(category, ': ', shapiro.p, '\n')
 }
 
-
-#drop outlier -> GenericAttachementMedia
-fb_tolpa <- fb_tolpa[-58, ]
 for (category in unique(fb_tolpa$'typ posta')){
   typ <- subset(fb_tolpa, `typ posta` == category)
   shapiro.p <- shapiro.test(typ$'suma reakcji')$p.value
